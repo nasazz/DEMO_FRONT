c9cdca2daae85dcb181e1ca521ce34e919836de2
nasazz <anasazzouz2002@gmail.com>
2025-09-28 19:46:38 +0100

    primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
+// ...existing code...
 import { ApplicationConfig } from '@angular/core';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideCore } from './core/core';
+
+// import your preset exported from theming/mytheme.ts
+import { MyPreset } from '../../theming/mytheme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideCore({ routes })],
+    providers: [
+        provideAnimationsAsync(),
+        provideRouter(routes),
+        providePrimeNG({
+            theme: {
+               preset: MyPreset,
+            }
+        })
+    ]
 };
